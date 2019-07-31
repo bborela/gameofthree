7f609acb8716b1755e61f376687ff48a7d4f2a0e
Bruno Borela <Bruno_Borela@epam.com>
2019-07-31 14:05:24 +0200

    Implemented GameClient class

---
+++ client/src/index.ts
@@ -0,0 +1,5 @@
+import { GameClient } from './game-client';
+
+let app = new GameClient('http://localhost:8080');
+
+export { app };
--- src/server.ts
+++
@@ -1,3 +0,0 @@
-setTimeout(() => {
-    console.log('test a');
-}, 2000);
